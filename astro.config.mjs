b4c1f506fdbcd1ed3a777635cdcf7f0bcadd545b
Jesse Koldewijn <jesse.koldewijn@justeattakeaway.com>
2025-04-07 00:48:20 +0200

    feat: styling tweaks, setup cleanup, deps upgrade and static convert

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from "astro/config";
 // Integrations
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
 import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
@@ -13,11 +12,7 @@ import prefetch from "@astrojs/prefetch";
 // https://astro.build/config
 export default defineConfig({
   site: "https://gt-dev.vercel.app",
-  output: "hybrid",
-  experimental: {
-    hybridOutput: true,
-  },
-  adapter: vercel(),
+  output: "static",
   integrations: [
     tailwind(),
     react(),
--- src/layouts/navbar/themeToggle.tsx
+++ src/layouts/navbar/themeToggle.tsx
@@ -1,17 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 
-import { Sun, Moon } from "lucide-react"
+import { Sun, Moon } from "lucide-react";
 
 const ThemeToggle = () => {
   type theme = "light" | "dark";
-  const [currentTheme, setCurrentTheme] = useState<theme>("dark");
+  const [currentTheme, setCurrentTheme] = useState<theme>(null);
 
   useEffect(() => {
     if (localStorage.getItem("theme")) {
       const currentTheme = localStorage.getItem("theme") as theme;
-      setCurrentTheme(currentTheme);
+      setCurrentTheme(currentTheme == "light" ? "light" : "dark");
     }
-  });
+  }, []);
 
   const setThemeToggle = () => {
     const htmlRoot = document.querySelector("html");
@@ -30,10 +30,17 @@ const ThemeToggle = () => {
     <>
       <div className="inline-block w-10">
         <button
-          className="btn-base-100 rounded-btn btn h-10 w-10 transition-all duration-300 ease-in-out"
+          className="flex h-10 w-10 items-center justify-center rounded-md border border-primary bg-primary-content text-primary transition-all duration-300 ease-in-out"
           onClick={setThemeToggle}
         >
-          <span className="m-auto text-3xl">
+          <span
+            className="m-auto text-3xl"
+            style={{
+              opacity: !currentTheme ? 0 : 1,
+              transform: !currentTheme ? "rotate(180deg)" : "rotate(0deg)",
+              transition: "transform 0.3s ease-in-out",
+            }}
+          >
             {currentTheme == "dark" ? <Sun /> : <Moon />}
           </span>
           <span className="sr-only">Theme toggle</span>
